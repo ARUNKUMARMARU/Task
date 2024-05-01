51c73970c40321d67ba9dcd1fc8f3795b4ca97af
ARUNKUMARMARU <130632965+ARUNKUMARMARU@users.noreply.github.com>
2024-05-01 16:26:33 +0530

    Update random.js

--- day 19 task/random dog pic/day 19 task/random dog pic/random.js
+++ day 19 task/random dog pic/day 19 task/random dog pic/random.js
@@ -1,16 +1,23 @@
-let randomLink = document.getElementById("randomLink")
-let img = document.getElementsByClassName("card-img-top")
+document.addEventListener("DOMContentLoaded", () => {
+    let randomLink = document.getElementById("randomLink");
+    let img = document.querySelector(".card-img-top");
+    let loadingSymbol = document.getElementById("loadingSymbol");
 
-randomLink.addEventListener("click", async(e)=>{
-    e.preventDefault()
-    let response=await fetch("https://random.dog/woof.json");
-    let data= await response.json();
-    
-    let link = data.url
-
-    console.log(img)
-    // randomLink.appendChild(link);
-    img[0].src=link
-
-})
-
+    randomLink.addEventListener("click", async(e) => {
+        e.preventDefault();
+        randomLink.style.display = 'none';
+        loadingSymbol.style.display = "inline-block";
+        try {
+            let response = await fetch("https://dog.ceo/api/breeds/image/random");
+            let data = await response.json();
+            let link = data.message;
+            img.src = link;
+        } catch (error) {
+            console.error('Error fetching random dog image:', error);
+        } finally {
+           
+            randomLink.style.display = 'inline-block';
+            loadingSymbol.style.display = "none";
+        }
+    });
+});
